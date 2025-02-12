4ab97ac377a64214fef735d5e8a014e61c603ce1
Nafi Sadman Khan <123958383+nafisadmankhan@users.noreply.github.com>
2025-02-12 23:04:45 +0600

    File updated at 2/12/2025, 11:04:45 PM

--- 2877-CreateaDataFramefromList/2877-CreateaDataFramefromList.py
+++ 2877-CreateaDataFramefromList/2877-CreateaDataFramefromList.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
-    df=pd.DataFrame(student_data,columns=['student_id','age'])
+    df = pd.DataFrame(student_data, columns=['student_id', 'age'])
     return df
     
