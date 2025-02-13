acab4295a3e2bf29087a0120a4612fb4d7716280
Nafi Sadman Khan <123958383+nafisadmankhan@users.noreply.github.com>
2025-02-13 13:37:14 +0600

    File updated at 2/13/2025, 1:37:14 PM

--- 2891-MethodChaining/2891-MethodChaining.py
+++ 2891-MethodChaining/2891-MethodChaining.py
@@ -1,1 +1,5 @@
-filtered_animals = animals[animals['weight'] > 100]
+import pandas as pd
+
+def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
+    filtered_animals=animals[animals['weight']>100].sort_values(by='weight',ascending=False)
+    return pd.DataFrame(filtered_animals['name'])
