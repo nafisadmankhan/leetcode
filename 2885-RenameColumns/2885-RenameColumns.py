97d3a2065522f82c176d52e8d5c543df12246c5a
Nafi Sadman Khan <123958383+nafisadmankhan@users.noreply.github.com>
2025-02-13 13:33:02 +0600

    File updated at 2/13/2025, 1:33:02 PM

--- 2885-RenameColumns/2885-RenameColumns.py
+++ 2885-RenameColumns/2885-RenameColumns.py
@@ -1,1 +1,5 @@
-DataFrame.astype(dtype, copy=True, errors='raise')
+import pandas as pd
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    students=students.rename(columns={'id':'student_id','first':'first_name','last':'last_name','age':'age_in_years'})
+    return students
