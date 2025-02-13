d13d022006c4fcd545fe273ebdd44fef7652078f
Nafi Sadman Khan <123958383+nafisadmankhan@users.noreply.github.com>
2025-02-13 13:35:59 +0600

    File updated at 2/13/2025, 1:35:58 PM

--- 2888-ReshapeData:Concatenate/2888-ReshapeData:Concatenate.py
+++ 2888-ReshapeData:Concatenate/2888-ReshapeData:Concatenate.py
@@ -1,2 +1,4 @@
+import pandas
+
 def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
-    return pd.concat([df1, df2], ignore_index=True)
+    return pandas.concat([df1,df2],axis=0)
